d8548543e76b9bfb25466afeba31dd97098496db
SyedZakariaRizvi <syedzakariarizvi@gmail.com>
2024-10-18 23:40:26 +0530

    feat: configure mongoose and setup user schema

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const express = require("express")
 const http = require("http")
 
@@ -5,6 +6,16 @@ const path = require("path")
 
 const app = express()
 const server = http.createServer(app)
+
+const mongoose = require("mongoose")
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("Database connected")
+    })
+    .catch((err) => {
+        console.log("Error connecting to database:", err)
+    })
 
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
