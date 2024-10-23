db480c8cf6132d75d96cfdaf11beb2c9aba8765c
SyedZakariaRizvi <syedzakariarizvi@gmail.com>
2024-10-23 08:55:02 +0530

    feat: add functionality to create new chat item

--- public/js/chats.js
+++ public/js/chats.js
@@ -6,3 +6,16 @@ chatItems.forEach(item => {
     this.classList.add('selected');
   });
 });
+
+document.querySelector("#create-chat-button").addEventListener("click", () => {
+  const userEmail = document.querySelector("#create-chat-input").value
+  
+  const newChatItem = document.createElement("div");
+  newChatItem.classList.add("chat-item");
+  newChatItem.setAttribute("data-email", userEmail);
+  
+  newChatItem.innerHTML = `<span>Loading...</span>`;
+
+  const displayChats = document.querySelector(".display-chats");
+  displayChats.insertBefore(newChatItem, displayChats.firstChild);
+})
